b8b5b6c282ae13dce59ce3271f554ce3a4facb49
NewCompte <victorvvv@gmail.com>
2014-01-27 00:08:47 +0100

    Amount of time with each power-up is now recorded. End of game time is now recorded correctly for players finishing the game. Indentation is now more consistent.

--- src/js/catstats.js
+++ src/js/catstats.js
@@ -18,40 +18,84 @@ catstats = (function(catstats) {
         .click(registerExport);
       $export.insertAfter($el);
     });
+	
+    function updateStatsAfterDeparture (player, now) {
+      var now = now || Date.now();
+      player["departure"] = tagpro.gameEndsAt - now;
+      if (player['bombtr']) {
+        player['bombtime'] += now - player['bombstart'];
+        player['bombtr'] = false;
+      }
+      if (player['tagprotr']) {
+        player['tagprotime'] += now - player['tagprostart'];
+        player['tagprotr'] = false;
+      }
+      if (player['griptr']) {
+        player['griptime'] += now - player['gripstart'];
+        player['griptr'] = false;
+      }
+      if (player['speedtr']) {
+        player['speedtime'] += now - player['speedstart'];
+        player['speedtr'] = false;
+      }
+    }
 
     tagpro.socket.on("p", function (newData) {
       newData = newData.u || newData;
       for(var i = 0; i < newData.length; i++) {
         var playerNewData = newData[i];
         var player = players[playerNewData.id];
+        var now = Date.now();
 
         if(!player) {
           player = players[playerNewData.id] = playerNewData;
-          player['arrival'] = tagpro.gameEndsAt - Date.now();
+          player['arrival'] = tagpro.gameEndsAt - now;
+          player['bombtime'] = 0;
+          player['tagprotime'] = 0;
+          player['griptime'] = 0;
+          player['speedtime'] = 0;
+          player['bombtr'] = false;
+          player['tagprotr'] = false;
+          player['griptr'] = false;
+          player['speedtr'] = false;
         }
 
         for(var statName in playerNewData) {
+          if (statName == 'bomb' || statName == 'tagpro' || statName == 'grip' || statName == 'speed') {
+            if (playerNewData[statName] && ! player[statName + "tr"]) { // the player has the statName powerup and we aren't tracking the amount of time with that powerup yet
+              player[statName+ "tr"] = true;
+              player[statName + 'start'] = now;
+            }
+            if (! playerNewData[statName] && player[statName + "tr"]) { // the player hasn't the statName powerup and we were tracking the amount of time with that power up.
+              player[statName + 'time'] += now - player[statName + 'start'];
+              player[statName+ "tr"] = false;
+            }
+          }
           player[statName] = playerNewData[statName];
         }
       }
     });
 
     tagpro.socket.on("playerLeft",function(playerId) {
-	    switch (tagpro.state) {
-	      case 1: //During the game
-		      players[playerId]["departure"] = tagpro.gameEndsAt - Date.now();
-		    break;
-		    case 3: //Before the game
-		      delete players[playerId];
-		    break;
-		    default:
-		    break;
-	    }
+      switch (tagpro.state) {
+        case 1: //During the game
+          updateStatsAfterDeparture(players[playerId]);
+        break;
+        case 3: //Before the game
+          delete players[playerId];
+        break;
+        default:
+        break;
+      }
     });
 
     tagpro.socket.on("time",function(e) {
       if(tagpro.state == 2) return; //Probably unneeded
       for(var playerId in players) players[playerId]["arrival"] = e.time; //players who were there before the game started have their arrival time set to the time when the game started
+    });
+    tagpro.socket.on('end', function () {
+      var now = Date.now();
+      for(var playerId in tagpro.players) updateStatsAfterDeparture(players[playerId], now); //tagpro.players is a list of all the players who are still in game
     });
     tagpro.socket.on('end', recordStats);
   }
@@ -89,7 +133,11 @@ catstats = (function(catstats) {
         'team captures':     player.team == 1 ? tagpro.score.r : tagpro.score.b,
         'opponent captures': player.team == 1 ? tagpro.score.b : tagpro.score.r,
         'arrival':           player['arrival']    || 0,
-        'departure':         player['departure']  || 0
+        'departure':         player['departure']  || 0,
+        'bombtime':          player['bombtime']   || 0,
+        'tagprotime':        player['tagprotime'] || 0,
+        'griptime':          player['griptime']   || 0,
+        'speedtime':         player['speedtime']  || 0
       }
     })
   }
